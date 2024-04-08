5134ec6646c8b6be2270ced98c4470e2a3ef5060
Allzer <109965453+Allzer@users.noreply.github.com>
2024-04-08 20:53:07 +0300

    сделал работу более корректной

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def generate_expression(target):
-    # Функция для генерации всех возможных комбинаций знаков +, -, и пустого места между числами
+def generate_expressions(target):
     def generate_combinations(n, expression=[]):
         if n == 0:
             combinations.append(expression)
@@ -8,17 +7,23 @@ def generate_expression(target):
         generate_combinations(n - 1, expression + ['+'])
         generate_combinations(n - 1, expression + ['-'])
 
-    numbers = list(range(9, -1, -1))  # Цифры от 9 до 0
+    def evaluate(expression):
+        expression = ''.join(str(num) + sign for num, sign in zip(numbers, expression)) + str(numbers[-1])
+        expression = expression.replace(' ', '')
+        return eval(expression)
+
+    numbers = list(range(9, -1, -1))
     combinations = []
     generate_combinations(len(numbers) - 1)
 
+    results = []
     for comb in combinations:
-        expression = ''.join(str(num) + sign for num, sign in zip(numbers, comb)) + str(numbers[-1])
-        expression = expression.replace(' ', '')  # Удаляем пустые промежутки
-        if eval(expression) == target:
-            return expression
+        if evaluate(comb) == target:
+            results.append(''.join(str(num) + sign for num, sign in zip(numbers, comb)) + str(numbers[-1]))
 
-    return "Невозможно найти комбинацию"
+    return results if results else ["Невозможно найти комбинацию"]
 
 target = 200
-print(generate_expression(target))
+expressions = generate_expressions(target)
+for expr in expressions:
+    print(expr.replace(' ', ''))
